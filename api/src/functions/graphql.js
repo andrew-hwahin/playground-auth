0b1038a5a661d1e530ce7e844e31df6c0b0883ed
Johan Eliasson <jeliasson@users.noreply.github.com>
2021-10-01 13:19:28 +0200

    Upgrading from 0.36.0 to 0.37.0

--- api/src/functions/graphql.js
+++ api/src/functions/graphql.js
@@ -4,7 +4,8 @@ import {
   makeServices,
 } from '@redwoodjs/graphql-server'
 
-import schemas from 'src/graphql/**/*.{js,ts}'
+import directives from 'src/directives/**/*.{js,ts}'
+import sdls from 'src/graphql/**/*.sdl.{js,ts}'
 import services from 'src/services/**/*.{js,ts}'
 
 import { getCurrentUser } from 'src/lib/auth.js'
@@ -13,14 +14,16 @@ import { logger } from 'src/lib/logger'
 
 export const handler = createGraphQLHandler({
   getCurrentUser,
+
   loggerConfig: {
     logger,
     options: { tracing: true, operationName: true, data: true },
   },
-  schema: makeMergedSchema({
-    schemas,
-    services: makeServices({ services }),
-  }),
+
+  directives,
+  sdls,
+  services,
+
   onException: () => {
     // Disconnect from your database with an unhandled exception.
     // Upgrade notes as of RedwoodJS 0.19.0, but this project does not use a db so we comment it out for now
